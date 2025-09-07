2312cc23cb9ebea0034e89c8ccbe3600478dc662
Pramod M B <mbpramod3@gmail.com>
2025-09-07 17:11:38 +0530

    Add files via upload

---
+++ snakegame/src/Main.java
@@ -0,0 +1,25 @@
+import javax.swing.*;
+
+public class Main {
+  public static void main(String[] args) throws Exception {
+    int boardWidth = 600;
+    int boardHeight = boardWidth;
+
+    JFrame frame = new JFrame("Snake");
+    frame.setVisible(true);
+    frame.setSize(boardWidth, boardHeight);
+    frame.setLocationRelativeTo(null);
+    frame.setResizable(false);
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    snakegame snakeGame = new snakegame(boardWidth, boardHeight);
+    frame.add(snakeGame);
+    frame.pack();
+    snakeGame.requestFocus();
+
+    
+    
+  }
+}
+
+
